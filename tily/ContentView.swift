c9c145d16d6a74d626e03ba6cb342f3754f9da01
evelyn <charlesdelorean1@icloud.com>
2021-05-23 21:07:56 -0400

    Initial Commit

---
+++ tily/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  tily
+//
+//  Created by charles on 2021-05-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
